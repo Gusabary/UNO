85a84d2b3c20bef8cd768ad412d55a660bab2fc5
Gusabary <gusabary@126.com>
2020-11-28 12:24:20 +0000

    feat: implement a bot a little bit more clever

--- bot/bot.py
+++ bot/bot.py
@@ -1,5 +1,6 @@
 from subprocess import Popen, PIPE
 import os
+import re
 import util
 
 class Bot:
@@ -10,28 +11,77 @@ class Bot:
         
         self.game = Popen(args, stdin=PIPE, stdout=PIPE)
         self.player_num = player_num
+        
+        # self.hint_stat = 0
+        self.last_played_card = None
         self.handcards = []
+        self.cursor_index = 0
+
+        self.game_loop()
     
-    def Input(self, action):
+    def press_keyboard(self, action=' '):
+        if action == ' ':
+            print("press: <space>")
+        elif action == '\n':
+            print("press: \\n")
+        else:
+            print("press:", action)
+
         action = bytes(action, "UTF-8")
         self.game.stdin.write(action)
         self.game.stdin.flush()
     
-    def Output(self):
-        line_num = util.get_ui_line_nums(self.player_num, len(self.handcards))
-        # print(line_num)
-        for row in range(line_num):
-            line = self.game.stdout.readline().decode("UTF-8") \
-                    .replace("\x1b[31m", "") \
-                    .replace("\x1b[32m", "") \
-                    .replace("\x1b[33m", "") \
-                    .replace("\x1b[34m", "") \
-                    .replace("\x1b[0m", "") \
-                    .replace("\n", "")
-            print(line)
+    def game_loop(self):
+        hint_stat = -1
+        last_hint_stat = -1
+        while True:
+            frame_info = util.next_frame(self.game, self.player_num)
+            print(frame_info)
+            if frame_info:
+                # it's my turn
+                hint_stat, self.last_played_card, self.handcards, self.cursor_index = frame_info
+                if hint_stat != last_hint_stat:
+                    # see something new, need time to think
+                    time_to_think = 2
+                    last_hint_stat = hint_stat
+                
+                if hint_stat == 4:
+                    # always want to play again
+                    self.press_keyboard('y')
+                    continue
 
-bot = Bot("localhost:9091")
-while True:
-    bot.Input(' ')
-    bot.Output()
-    # print("*******")
+                if time_to_think == 0:
+                    # ok, do some action
+                    if hint_stat == 1:
+                        self.try_play_card()
+                    elif hint_stat == 2:
+                        self.press_keyboard(' ')
+                    elif hint_stat == 3:
+                        self.press_keyboard('r')
+                    else:
+                        assert False
+                else:
+                    time_to_think -= 1
+
+    def try_play_card(self):
+        # press keyboard only once for each frame
+        index_of_card_to_play = -1
+        for i in range(len(self.handcards)):
+            if util.can_be_played_after(self.handcards[i], self.last_played_card):
+                index_of_card_to_play = i
+                break
+        print("index of card to play:", index_of_card_to_play, "\tcursor index:", self.cursor_index)
+        if index_of_card_to_play == -1:
+            # no card can be played
+            self.press_keyboard(' ')
+        else:
+            # there is a card to play
+            if index_of_card_to_play < self.cursor_index:
+                self.press_keyboard(',')
+            elif index_of_card_to_play > self.cursor_index:
+                self.press_keyboard('.')
+            else:
+                self.press_keyboard('\n')
+        
+
+bot = Bot("localhost:9091", player_num=3)
--- bot/util.py
+++ bot/util.py
@@ -1,7 +1,105 @@
+import re
 import conf
 
 def get_ui_line_nums(player_num, handcards_num):
     base_line_num = conf.base_scale_of_view[player_num][0]
-    # -1 // 8 == 0 in python
+    # -1 // 8 == -1 in python
     extra_line_num = max((handcards_num - 1) // 8, 0)
     return base_line_num + extra_line_num
+
+def remove_escape(s):
+    return s.replace("\x1b[31m", "") \
+            .replace("\x1b[32m", "") \
+            .replace("\x1b[33m", "") \
+            .replace("\x1b[34m", "") \
+            .replace("\x1b[0m", "") 
+
+def can_be_played_after(card_to_play, last_played_card):
+    _card_to_play = remove_escape(card_to_play)
+    _last_played_card = remove_escape(last_played_card)
+
+    # print("A:", _card_to_play, "\tB,", _last_played_card)
+    if _card_to_play == 'W' or _card_to_play == '+4':
+        return False
+    if _last_played_card[1:] == 'S' or _last_played_card[1:] == '+2' or _last_played_card[1:] == '+4':
+        return False
+    if _card_to_play[0] == _last_played_card[0]:
+        return True
+    if len(_last_played_card) > 1 and _card_to_play[1] == _last_played_card[1]:
+        return True
+
+def update_handcards(handcards, line):
+    cursor_index = -1
+    _line = line.split()[1:-1]
+    for i in range(len(_line)):
+        if _line[i][0] == '>':
+            cursor_index = i
+            _line[i] = _line[i][1:]
+            break
+
+    return handcards + _line, cursor_index
+
+def next_frame(game, player_num):
+    cur_line_num = 0
+    is_updating_handcards = False
+    is_my_turn = False
+    stat = 0
+    lines_left_num = -1
+
+    # return value
+    last_played_card = None
+    handcards = []
+    cursor_index = -1
+
+    _pos_of_last_played_card = conf.pos_of_last_played_card[player_num]
+    _pos_of_my_box = conf.pos_of_player_box[player_num][0]
+
+    while True:
+        line = game.stdout.readline().decode("UTF-8").replace("\n", "") 
+        line = remove_escape(line)
+        print(cur_line_num, "\t", line)
+
+        if cur_line_num == 0 and re.match(".*Want to play again", line):
+            stat = 4
+            return stat, last_played_card, handcards, cursor_index
+
+        if cur_line_num == _pos_of_last_played_card[0]:
+            # last played card
+            if len(line[_pos_of_last_played_card[1]:].split()) > 0:
+                last_played_card = line[_pos_of_last_played_card[1]:].split()[0].strip()
+        
+        if cur_line_num == _pos_of_my_box[0] + 3:
+            # handcards
+            is_updating_handcards = True
+
+        if is_updating_handcards:
+            if re.match(".*\+--------", line):
+                is_updating_handcards = False
+            else:
+                handcards, _cursor_index = update_handcards(handcards, line)
+                if _cursor_index > -1:
+                    cursor_index = _cursor_index
+
+        if cur_line_num == get_ui_line_nums(player_num, len(handcards)) - 1:
+            if line[_pos_of_my_box[1]] != '[':
+                # it's not my turn
+                return False
+
+        if cur_line_num == get_ui_line_nums(player_num, len(handcards)):
+            if re.match("Now it's your turn", line):
+                lines_left_num = 3
+                stat = 1
+            elif re.match("Press Enter to play the card just drawn immediately", line):
+                lines_left_num = 2
+                stat = 2
+            elif re.match("Specify the next color", line):
+                lines_left_num = 1
+                stat = 3
+            else:
+                assert False
+        
+        lines_left_num -= 1
+        if lines_left_num == 0:
+            return stat, last_played_card, handcards, cursor_index
+
+        cur_line_num += 1
